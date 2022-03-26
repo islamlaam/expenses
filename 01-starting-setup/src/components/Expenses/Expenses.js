3974c7c2032bf672f6f18306270281e6a4d73f2f
islamlaam <islamlaam@gmail.com>
2022-03-26 21:23:28 +0400

    adding files

---
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -0,0 +1,35 @@
+import react, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+
+      {props.items.map((expense, index) => (
+        <ExpenseItem
+          key={index}
+          // key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
